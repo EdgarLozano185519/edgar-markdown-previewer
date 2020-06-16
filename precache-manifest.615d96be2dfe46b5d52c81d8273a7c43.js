4e11e805ac1f589d6525f0e884baeade2021671f
Edgar Lozano <edgar@edgarl.com>
2020-06-16 18:18:11 -0500

    Updates

---
+++ precache-manifest.615d96be2dfe46b5d52c81d8273a7c43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ed2c6b5f57362be50772fe0607217f",
+    "url": "/mdp/index.html"
+  },
+  {
+    "revision": "883587a07b18ccc05d4f",
+    "url": "/mdp/static/css/main.7b071f5e.chunk.css"
+  },
+  {
+    "revision": "9107f8a79a9a8591412f",
+    "url": "/mdp/static/js/2.d806ea85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mdp/static/js/2.d806ea85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883587a07b18ccc05d4f",
+    "url": "/mdp/static/js/main.1d1e3bae.chunk.js"
+  },
+  {
+    "revision": "b089088ad9d5dea7b8a3",
+    "url": "/mdp/static/js/runtime-main.0100c24c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mdp/static/media/logo.5d5d9eef.svg"
+  }
+]);
